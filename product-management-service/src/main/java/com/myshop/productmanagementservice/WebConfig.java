6d4de0f48c1f3519020ccd5f2a6cb37c60bbf40c
mathieu <matjuillard@hotmail.com>
2019-10-20 16:26:44 +0200

    test

--- product-management-service/src/main/java/com/myshop/productmanagementservice/WebConfig.java
+++ product-management-service/src/main/java/com/myshop/productmanagementservice/WebConfig.java
@@ -1,7 +1,9 @@
 package com.myshop.productmanagementservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private Environment env;
 
 	@Value("${frontManagement.url}")
 	private String allowedOrigin;
